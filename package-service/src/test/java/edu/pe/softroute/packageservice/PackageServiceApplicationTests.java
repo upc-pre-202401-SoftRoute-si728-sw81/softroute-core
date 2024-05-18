81e39a19a39d4f0bac7a40892f51b9467f723519
diegoda04 <u20201c334@upc.edu.pe>
2024-05-17 19:35:34 -0500

    first commit

---
+++ package-service/src/test/java/edu/pe/softroute/packageservice/PackageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.softroute.packageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
