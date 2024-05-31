d5b9eb8f4d46bb7b597886e07cad2cb4eb4263c8
diegoda04 <u20201c334@upc.edu.pe>
2024-05-31 14:09:58 -0500

    feat(tracking-service): added web socket endpoint for tracking

--- tracking-service/src/main/java/edu/pe/softroute/trackingservice/infrastructure/messaging/consumer/IotDataConsumer.java
+++ tracking-service/src/main/java/edu/pe/softroute/trackingservice/infrastructure/messaging/consumer/IotDataConsumer.java
@@ -3,11 +3,13 @@ package edu.pe.softroute.trackingservice.infrastructure.messaging.consumer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.pe.softroute.trackingservice.domain.models.entities.Location;
+import edu.pe.softroute.trackingservice.domain.models.entities.Tracking;
 import edu.pe.softroute.trackingservice.domain.services.TrackingService;
 import edu.pe.softroute.trackingservice.infrastructure.messaging.consumer.dto.IotData;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,15 +17,18 @@ import org.springframework.stereotype.Component;
 public class IotDataConsumer {
 
   private final TrackingService trackingService;
+  private final SimpMessagingTemplate messagingTemplate;
 
-  @RabbitListener(queues = "iot.queue")
+  @RabbitListener(queues = "iot.queue", autoStartup = "false")
   public void handleMessage(String message) throws JsonProcessingException {
     ObjectMapper objectMapper = new ObjectMapper();
     IotData data = objectMapper.readValue(message, IotData.class);
+
     System.out.println("Received message from queue " + "iot.queue" + ": " + data);
 
     Location location = new Location(data.getLatitude(), data.getLongitude(), LocalDateTime.now());
 
-    trackingService.updateLocationByTrackingNumber(data.getTrackingNumber(), location);
+    Tracking tracking = trackingService.updateLocationByTrackingNumber(data.getTrackingNumber(), location);
+    messagingTemplate.convertAndSend("/topic/tracking", objectMapper.writeValueAsString(tracking));
   }
 }
