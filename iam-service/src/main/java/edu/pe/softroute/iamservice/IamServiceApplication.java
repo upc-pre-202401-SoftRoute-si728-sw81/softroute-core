3931e31918b9989bd277faf18dc821d452ad20ce
DiegoDA04 <u20201c334@upc.edu.pe>
2024-06-08 15:21:47 -0500

    feat: added Discovery Client anotation

--- iam-service/src/main/java/edu/pe/softroute/iamservice/IamServiceApplication.java
+++ iam-service/src/main/java/edu/pe/softroute/iamservice/IamServiceApplication.java
@@ -2,8 +2,10 @@ package edu.pe.softroute.iamservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IamServiceApplication {
 
 	public static void main(String[] args) {
