aad9f77285517bfe1294af9a0bb64df23f9ed39e
DiegoDA04 <u20201c334@upc.edu.pe>
2024-06-08 19:10:49 -0500

    feat(tracking-service): CORS CONFIGURATION

--- tracking-service/src/main/java/edu/pe/softroute/trackingservice/infrastructure/WebConfig.java
+++ tracking-service/src/main/java/edu/pe/softroute/trackingservice/infrastructure/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
 }
